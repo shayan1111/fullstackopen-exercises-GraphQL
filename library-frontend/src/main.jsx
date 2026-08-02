2a3472fe55c00737466e1674af9cc175696e755e
shayan1111 <69235308+shayan1111@users.noreply.github.com>
2026-08-02 13:46:07 +0330

    Update main.jsx

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -16,7 +16,7 @@ const authLink = new SetContextLink(({ headers }) => {
   };
 });
 
-const httpLink = new HttpLink({ uri: "http://localhost:4001" });
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
